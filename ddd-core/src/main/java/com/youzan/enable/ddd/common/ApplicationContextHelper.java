6419f84d0b2e4f73e40f7fee15595586ffb6501b
chuxiaofeng <chuxiaofeng@youzan.com>
2018-04-18 00:50:38 +0800

    ~

--- ddd-core/src/main/java/com/youzan/enable/ddd/common/ApplicationContextHelper.java
+++ ddd-core/src/main/java/com/youzan/enable/ddd/common/ApplicationContextHelper.java
@@ -1,5 +1,6 @@
 package com.youzan.enable.ddd.common;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -12,7 +13,7 @@ import org.springframework.stereotype.Component;
  * @date 2018-01-07 12:30 PM
  */
 @Component
-public class ApplicationContextHelper implements ApplicationContextAware{
+public class ApplicationContextHelper implements ApplicationContextAware {
 
     private static ApplicationContext applicationContext;
 
@@ -23,8 +24,16 @@ public class ApplicationContextHelper implements ApplicationContextAware{
         ApplicationContextHelper.applicationContext = applicationContext;
     }
 
-    public static Object getBean(Class claz){
-        return ApplicationContextHelper.applicationContext.getBean(claz);
+    public static <T> T getBean(@NotNull Class<T> requiredType){
+        return ApplicationContextHelper.applicationContext.getBean(requiredType);
+    }
+
+    public static <T> T getBean(Class<T> requiredType, Object... args) {
+        return ApplicationContextHelper.applicationContext.getBean(requiredType, args);
+    }
+
+    public static Object getBean(@NotNull String name, Object... args) {
+        return ApplicationContextHelper.applicationContext.getBean(name, args);
     }
 
     public static ApplicationContext getApplicationContext() {
